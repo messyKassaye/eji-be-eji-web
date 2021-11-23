dc534e55b466a4060b8999324732f4b389ef945e
Meseret <meseret.kassayes@gmail.com>
2021-11-23 15:34:43 +0300

    minor component addition

--- src/components/content/Categories.js
+++ src/components/content/Categories.js
@@ -2,5 +2,9 @@ import { Card } from "antd";
 import React from "react";
 
 export default function Categories() {
-  return <Card className="flex fixed p-10"></Card>;
+  return (
+    <Card title="Posts by category" className="flex flex-col fixed p-10">
+      <p>Hello world</p>
+    </Card>
+  );
 }
